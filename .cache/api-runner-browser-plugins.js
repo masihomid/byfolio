577d116290883bda9cd24453ef24b43182d911c7
darKoub <43557682+darKoub@users.noreply.github.com>
2020-01-19 21:59:14 -0500

    faulty

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,16 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"D:\\Code\\masih-portfolio\\node_modules\\@christiandavid\\gatsby-theme-byfolio/src/utils/typography"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Christian David Ibarguen","short_name":"CD","description":"This cool App contains information about my work experience as a software developer.","lang":"en","start_url":"/","background_color":"#000","theme_color":"#fff","display":"standalone","icon":"src/images/icon.png"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-transition-link/gatsby-browser.js'),
+      options: {"plugins":[],"layout":"D:\\Code\\masih-portfolio\\node_modules\\@christiandavid\\gatsby-theme-byfolio\\src\\layout\\index.js"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-nprogress/gatsby-browser.js'),
+      options: {"plugins":[],"showSpinner":false},
+    }]
---
+++ src/experience/_additionalSkills.md
@@ -0,0 +1,15 @@
+---
+title: "title"
+company: "impact"
+jobTitle: "ceo"
+logo: 
+skills:
+  [
+    { title: "React", image: ../images/skills/react.png },
+    { title: "Gatsby", image: ../images/skills/gatsby.png },
+    { title: "GraphQL", image: ../images/skills/graphql.png }
+  ]
+images: []
+dateFrom: "2019-09-01"
+dateTo: "2019-09-01"
+---
